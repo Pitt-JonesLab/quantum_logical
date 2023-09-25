8e49b4742e65fb2873df86ef51f2c2c9c21f52d5
Evan McKinney <evmckinney9@gmail.com>
2023-09-25 10:11:10 -0400

    testing g-f sqrtiswap decomps

--- src/quantum_logical/error_channel.py
+++ src/quantum_logical/error_channel.py
@@ -3,8 +3,8 @@
 # TODO implemented Generalized Amplitude Damping Channel
 # this considers "hot"-qubits, ground state is not perfectly |0>
 
-from abc import ABC, abstractmethod
+from abc import ABC
 
 
 class ErrorChannel(ABC):
-    def
+    pass
